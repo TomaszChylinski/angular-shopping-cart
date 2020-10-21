e3f93e97db43cc998c5487e6fe527daf8cb8eea5
chylinski.t <chylinski.t@outlook.com>
2020-10-20 22:26:49 -0400

    feat(added shop cart functionality

--- shopping-cart/src/app/components/shopping-cart/cart/cart.component.ts
+++ shopping-cart/src/app/components/shopping-cart/cart/cart.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { MessengerService } from 'src/app/services/messenger.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartItems = [
+    // { id: 1, productId: 1, qty: 4, productTitle: 'prod1', price: 100 },
+    // { id: 1, productId: 2, qty: 4, productTitle: 'prod12', price: 200 },
+    // { id: 1, productId: 3, qty: 4, productTitle: 'prod13', price: 300 },
+    // { id: 1, productId: 4, qty: 4, productTitle: 'prod14', price: 400 },
+  ];
 
-  constructor() { }
+  cartTotal = 0;
 
-  ngOnInit(): void {
+  constructor(private message: MessengerService) {}
+
+  ngOnInit() {
+    this.message.getMessage().subscribe((product: Product) => {
+      this.addProductToCart(product);
+    });
   }
 
+  addProductToCart(product: Product) {
+
+    let productExists = false;
+
+    for (const i in this.cartItems) {
+      if (this.cartItems[i].id === product.id) {
+        this.cartItems[i].qty++;
+        productExists = true;
+        break;
+      }
+    }
+
+    if (!productExists) {
+      this.cartItems.push({
+        id: product.id,
+        productTitle: product.name,
+        qty: 1,
+        price: product.price,
+      });
+    }
+  }
 }
--- shopping-cart/src/app/components/shopping-cart/list/product-list.component.ts
+++ shopping-cart/src/app/components/shopping-cart/list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductsService} from 'src/app/services/products.service';
+import {Product} from 'src/app/models/product'
 
 @Component({
   selector: 'app-list',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  productList: Product[] = [];
+  constructor(private productService: ProductsService) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+   this.productList = this.productService.getProducts();
   }
-
 }
