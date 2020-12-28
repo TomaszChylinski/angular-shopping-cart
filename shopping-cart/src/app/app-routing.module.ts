3affe279447e52c79b497803547fbd2f49483713
chylinski.t <chylinski.t@outlook.com>
2020-12-28 17:36:48 -0500

    update routing

--- shopping-cart/src/app/app-routing.module.ts
+++ shopping-cart/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -13,6 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'shop', component: ShoppingCartComponent },
+  { path: 'wish', component: WishListComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', component: PageNotFoundComponent }, // ** match every route - if nothing else aboe not matches this route will be hit.
 ];
